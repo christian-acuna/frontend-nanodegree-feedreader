1b523ecc4f0034ed63b60ec84b2c2f49a975674e
Christian Acuna <cacuna0828@gmail.com>
2016-10-16 12:54:03 -0700

    add tests for each RSS Feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,23 +26,29 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* This test  loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('has a URL defined and is not empty', function () {
-           allFeeds.forEach(function(feed) {
-             expect(feed.url).toBeDefined();
-             expect(feed.url.length).not.toEqual(0);
+        describe("Each RSS Feed", function () {
+          /* This test  loops through each feed
+           * in the allFeeds object and ensures it has a URL defined
+           * and that the URL is not empty.
+           */
+           it('has a URL defined and is not empty', function () {
+             allFeeds.forEach(function(feed) {
+               expect(feed.url).toBeDefined();
+               expect(feed.url.length).not.toEqual(0);
+             });
            });
-         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+          /* This test loops through each feed
+           * in the allFeeds object and ensures it has a name defined
+           * and that the name is not empty.
+           */
+          it('has a name and is not empty', function () {
+            allFeeds.forEach(function(feed) {
+              expect(feed.name).toBeDefined();
+              expect(feed.name.length).not.toEqual(0);
+            });
+          });
+        });
     });
 
 
